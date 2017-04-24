6a49a2521a31fb5caeebc195d527e4b2c7e87934
hungmi <hungmihungmi@gmail.com>
2017-04-25 00:54:25 +0800

    add nginx.conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,6 +41,7 @@ namespace :deploy do
       upload! StringIO.new(File.read("config/database.yml")), "#{shared_path}/config/database.yml"
       upload! StringIO.new(File.read("config/application.yml")), "#{shared_path}/config/application.yml"
       upload! StringIO.new(File.read("config/application.yml")), "#{shared_path}/config/secrets.yml"
+      upload! StringIO.new(File.read("config/nginx.conf")), "#{shared_path}/config/nginx.conf"
     end
   end
 end
